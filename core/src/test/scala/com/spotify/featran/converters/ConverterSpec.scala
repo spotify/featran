21531f62b6b66c34cdc742d27e71101e04af12fc
Neville Li <neville@spotify.com>
2017-05-23 11:28:33 -0400

    add boolean converters

---
+++ core/src/test/scala/com/spotify/featran/converters/ConverterSpec.scala
@@ -0,0 +1,28 @@
+/*
+ * Copyright 2017 Spotify AB.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package com.spotify.featran.converters
+
+import org.scalacheck._
+
+object ConverterSpec extends Properties("converters") {
+
+  property("boolean.asDouble") = Prop.forAll { x: Boolean =>
+    x.asDouble == (if (x) 1.0 else 0.0)
+  }
+
+}
