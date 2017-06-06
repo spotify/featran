54f7624e72ee6010d367ab42b182039e4e8e0a03
Neville Li <neville@spotify.com>
2017-06-06 17:27:53 -0400

    add TensorFlow Example FeatureBuilder

---
+++ tensorflow/src/main/scala/com/spotify/featran/tensorflow/package.scala
@@ -0,0 +1,35 @@
+/*
+ * Copyright 2017 Spotify AB.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package com.spotify.featran
+
+import org.tensorflow.example.{Example, Feature, Features, FloatList}
+
+package object tensorflow {
+  implicit val tfExampleFB: FeatureBuilder[Example] = new FeatureBuilder[Example] {
+    private val fb = Features.newBuilder()
+    override def init(dimension: Int): Unit = fb.clear()
+    override def add(name: String, value: Double): Unit =
+      fb.putFeature(
+        name,
+        Feature.newBuilder()
+          .setFloatList(FloatList.newBuilder().addValue(value.toFloat))
+          .build())
+    override def skip(): Unit = Unit
+    override def result: Example = Example.newBuilder().setFeatures(fb).build()
+  }
+}
---
+++ tensorflow/src/test/scala/com/spotify/featran/tensorflow/TensorFlowFeatureBuilderSpec.scala
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2017 Spotify AB.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package com.spotify.featran.tensorflow
+
+import com.spotify.featran.FeatureBuilder
+import org.scalacheck._
+import org.tensorflow.example.{Example, Feature, Features, FloatList}
+
+object TensorFlowFeatureBuilderSpec extends Properties("TensorFlowFeatureBuilder") {
+
+  private def list[T](implicit arb: Arbitrary[Option[T]]): Gen[List[Option[T]]] =
+    Gen.listOfN(100, arb.arbitrary)
+
+  property("TensorFlow Example") = Prop.forAll(list[Double]) { xs =>
+    val fb = implicitly[FeatureBuilder[Example]]
+    fb.init(xs.size)
+    val b = Features.newBuilder()
+    xs.zipWithIndex.foreach {
+      case (Some(x), i) =>
+        val key = "key" + i.toString
+        fb.add(key, x)
+        b.putFeature(
+          key,
+          Feature.newBuilder().setFloatList(FloatList.newBuilder().addValue(x.toFloat)).build())
+      case (None, _) => fb.skip()
+    }
+    fb.result == Example.newBuilder().setFeatures(b).build()
+  }
+
+}
