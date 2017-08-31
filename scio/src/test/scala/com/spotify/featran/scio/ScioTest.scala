29bd46d514ef7c080d009288aa91fcc1e688e0c7
Neville Li <neville@spotify.com>
2017-08-31 16:28:53 -0400

    add cross build settings, fix #3

--- scio/src/test/scala/com/spotify/featran/scio/ScioTest.scala
+++ scio/src/test/scala/com/spotify/featran/scio/ScioTest.scala
@@ -55,8 +55,7 @@ class ScioTest extends PipelineSpec {
         .required(e => foo.method(e._1))(Identity("foo"))
         .extract(sc.parallelize(testData))
 
-      val thrown = the [RuntimeException] thrownBy f.featureValues[Seq[Double]]
-      thrown.getMessage should startWith("unable to serialize anonymous function")
+      an [Exception] should be thrownBy f.featureValues[Seq[Double]]
     }
   }
   // scalastyle:on no.whitespace.before.left.bracket
