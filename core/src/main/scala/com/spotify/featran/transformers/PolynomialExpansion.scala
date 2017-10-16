51de572aef346b4f0469cc49bfdd88721448c206
Neville Li <neville@spotify.com>
2017-10-16 16:26:31 +0200

    port CombinatoricsUtils and remove commons-math3, fix #54

--- core/src/main/scala/com/spotify/featran/transformers/PolynomialExpansion.scala
+++ core/src/main/scala/com/spotify/featran/transformers/PolynomialExpansion.scala
@@ -19,7 +19,6 @@ package com.spotify.featran.transformers
 
 import com.spotify.featran.{FeatureBuilder, FeatureRejection}
 import com.twitter.algebird.Aggregator
-import org.apache.commons.math3.util.CombinatoricsUtils
 
 /**
  * Transform vector features by expanding them into a polynomial space, which is formulated by an
@@ -106,4 +105,188 @@ private class PolynomialExpansion(name: String, val degree: Int, val expectedLen
   override def params: Map[String, String] = Map(
     "degree" -> degree.toString,
     "expectedLength" -> expectedLength.toString)
+}
+
+// Ported from commons-math3
+private object CombinatoricsUtils {
+  // scalastyle:off cyclomatic.complexity
+  // scalastyle:off method.length
+  def binomialCoefficient(n: Int, k: Int): Long = {
+    checkBinomial(n, k)
+    if (n == k || k == 0) {
+      1
+    } else if (k == 1 || k == n - 1) {
+      n
+    } else if (k > n / 2) {
+      // Use symmetry for large k
+      binomialCoefficient(n, n - k)
+    } else {
+      // We use the formula
+      // (n choose k) = n! / (n-k)! / k!
+      // (n choose k) == ((n-k+1)*...*n) / (1*...*k)
+      // which could be written
+      // (n choose k) == (n-1 choose k-1) * n / k
+      var result = 1L
+      if (n <= 61) {
+        // For n <= 61, the naive implementation cannot overflow.
+        var i = n - k + 1
+        var j = 1
+        while (j <= k) {
+          result = result * i / j
+          i += 1
+          j += 1
+        }
+      } else if (n <= 66) {
+        // For n > 61 but n <= 66, the result cannot overflow,
+        // but we must take care not to overflow intermediate values.
+        var i = n - k + 1
+        var j = 1
+        while (j <= k) {
+          // We know that (result * i) is divisible by j,
+          // but (result * i) may overflow, so we split j:
+          // Filter out the gcd, d, so j/d and i/d are integer.
+          // result is divisible by (j/d) because (j/d)
+          // is relative prime to (i/d) and is a divisor of
+          // result * (i/d).
+          val d = gcd(i, j)
+          result = (result / (j / d)) * (i / d)
+          i += 1
+          j += 1
+        }
+      } else {
+        // For n > 66, a result overflow might occur, so we check
+        // the multiplication, taking care to not overflow
+        // unnecessary.
+        var i = n - k + 1
+        var j = 1
+        while (j <= k) {
+          val d = gcd(i, j)
+          result = mulAndCheck(result / (j / d), i / d)
+          i += 1
+          j += 1
+        }
+      }
+      result
+    }
+  }
+  // scalastyle:on method.length
+  // scalastyle:on cyclomatic.complexity
+
+  def gcd(p: Int, q: Int): Int = {
+    if (p == 0 || q == 0) {
+      require(p != Int.MinValue && q != Int.MinValue, s"overflow: gcd($p, $q) is 2^31")
+      abs(p + q)
+    } else {
+      var a = p
+      var b = q
+      var al: Long = a
+      var bl: Long = b
+      var useLong = false
+      if (a < 0) {
+        if (a == Int.MinValue) {
+          useLong = true
+        } else {
+          a = -a
+        }
+        al = -al
+      }
+      if (b < 0) {
+        if (b == Int.MinValue) {
+          useLong = true
+        } else {
+          b = -b
+        }
+        bl = -bl
+      }
+      if (useLong) {
+        require(al != bl, s"overflow: gcd($p, $q) is 2^31")
+      }
+      var blbu = bl
+      bl = al
+      al = blbu % al
+      if (al == 0) {
+        require(bl <= Int.MaxValue, s"overflow: gcd($p, $q) is 2^31")
+        bl.toInt
+      } else {
+        blbu = bl
+
+        // Now "al" and "bl" fit in an "int".
+        b = al.toInt
+        a = (blbu % al).toInt
+        gcdPositive(a, b)
+      }
+    }
+  }
+
+  private def gcdPositive(p: Int, q: Int): Int = {
+    // assert q != 0
+    if (p == 0) {
+      q
+    } else {
+      var a = p
+      var b = q
+      val aTwos = Integer.numberOfTrailingZeros(a)
+      a = a >> aTwos
+      val bTwos = Integer.numberOfTrailingZeros(b)
+      b = b >> bTwos
+      val shift = if (aTwos <= bTwos) aTwos else bTwos
+      // "a" and "b" are positive.
+      // If a > b then "gdc(a, b)" is equal to "gcd(a - b, b)".
+      // If a < b then "gcd(a, b)" is equal to "gcd(b - a, a)".
+      // Hence, in the successive iterations:
+      //  "a" becomes the absolute difference of the current values,
+      //  "b" becomes the minimum of the current values.
+      while (a != b) {
+        val delta = a - b
+        b = Math.min(a, b)
+        a = Math.abs(delta)
+
+        // Remove any power of 2 in "a" ("b" is guaranteed to be odd).
+        a >>= Integer.numberOfTrailingZeros(a)
+      }
+
+      // Recover the common power of 2.
+      a << shift
+    }
+  }
+
+  def mulAndCheck(a: Long, b: Long): Long = {
+    if (a > b) {
+      // use symmetry to reduce boundary cases
+      mulAndCheck(b, a)
+    } else {
+      if (a < 0) {
+        if (b < 0) {
+          // check for positive overflow with negative a, negative b
+          require(a >= Long.MaxValue / b)
+          a * b
+        } else if (b > 0) {
+          // check for negative overflow with negative a, positive b
+          require(a >= Long.MinValue / b)
+          a * b
+        } else {
+          // assert b == 0
+          0
+        }
+      } else if (a > 0) {
+        // assert a > 0
+        // assert b > 0
+
+        // check for positive overflow with positive a, positive b
+        require(a <= Long.MaxValue / b)
+        a * b
+      } else {
+        // assert a == 0
+        0
+      }
+    }
+  }
+
+  @inline
+  def abs(x: Int): Int =  (x ^ (~(x >>> 31) + 1)) + (x >>> 31)
+
+  private def checkBinomial(n: Int, k: Int): Unit = {
+    require(n >= k, s"must have n >= k for binomial coefficient (n, k), got k = $k, n = $n")
+    require(n >= 0, s"must have n >= 0 for binomial coefficient (n, k), got n = $n")
+  }
 }
